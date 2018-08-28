8ee9cf4c6504421ccc2211f741dd054b6cdf4c94
mozzi0131 <sue03094@gmail.com>
2018-08-28 17:55:40 +0900

    editing pythondbtest.py
    데이터베이스 inserting 관련 함수 완료
    - 현재 구현은 가장 최근에 들어온 한 행과만 비교해서 데이터 넣게 함 >> 나중에 데이터 select하는 방식은 where clause 넣어서 수정할 여지 있음
    - 그렇게 맘에는 안듬 좀더 깔끔하게 짤 수 있는 방법 찾아보기

--- pythondbtest.py
+++ pythondbtest.py
@@ -1,14 +1,16 @@
 import sqlite3 as s
 import datetime
 
-data = 2
+#data = 1
 
-if __name__ == "__main__":
-#def insertData(data):
+#if __name__ == "__main__":
+def insertData(data):
     with s.connect(r"database.db") as conn:
         cur = conn.cursor()
         insert_sql = "insert into classifiedsound (soundIndex,time) values (?,?)"
-        select_sql = "select soundIndex,time from classifiedsound order by time desc limit 1"
+        select_sql = "select * from classifiedsound order by time desc limit 1"
+
+        test_sql = "select * from classifiedsound order by time"
  
     # 1건씩 추가하기
     #여기서 이전 데이터와 비교해서?? 최근 것과 큰 시간차없이 같은 data 들어가면 pass하기
@@ -23,30 +25,48 @@ if __name__ == "__main__":
             latest_data = i
 
         if latest_data[0] == data :
-            latest_time = latest_data[1]
-            latest_hour = latest_time.split(':')[0]
-            latest_minute = latest_time.split(':')[1]
-            latest_second = latest_time.split(':')[2]
+            latest_time = latest_data[1].split(' ')[1]
+            latest_hour = (int)(latest_time.split(':')[0])
+            latest_minute = (int)(latest_time.split(':')[1])
+            latest_second = (int)(latest_time.split(':')[2])
 
             latest_cal = latest_hour*3600 + latest_minute*60 + latest_second
+            print(latest_cal)
 
             dateInfo = str(datetime.datetime.now()).split('.')[0]
             print(dateInfo)
             now_time = dateInfo.split(' ')[1]
-            now_hour = now_time.split(':')[0]
-            now_minute = now_time.split(':')[1]
-            now_second = now_time.split(':')[2]
+            now_hour = (int)(now_time.split(':')[0])
+            now_minute = (int)(now_time.split(':')[1])
+            now_second = (int)(now_time.split(':')[2])
 
             now_cal = now_hour*3600 + now_minute*60 + now_second
+            print(now_cal)
 
-            if latest_cal - now_cal <= 300:
-                break;
-            else :
-                cur.execute(insert_sql, (data,dateInfo)
+            #5분을 초과한 차이가 있을 경우
+            if latest_cal - now_cal > 300:
+                cur.execute(insert_sql, (data,dateInfo))
                 conn.commit()
+
+                cur.execute(test_sql)
+                t_result = cur.fetchall()
+
+                for j in t_result:
+                    print(j)
+                
+               
                 cur.close()
+            else:
+                print("같은 소리가 반복적으로 입력되고 있습니다.")
 
         else :
             cur.execute(insert_sql, (data,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
             conn.commit()
+
+            cur.execute(test_sql)
+            t_result = cur.fetchall()
+
+            for j in t_result:
+                print(j)
+                    
             cur.close()
